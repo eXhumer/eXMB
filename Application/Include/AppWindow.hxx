8f2dcafb1f37f29e73df5b4c63209ca7d71d86c7
eXhumer <exhumer@exhumer.cc>
2022-01-06 07:24:33 -0700

    feat: Add new WIP features to application

--- Application/Include/AppWindow.hxx
+++ Application/Include/AppWindow.hxx
@@ -5,20 +5,42 @@ namespace eXRC::Service {
 class Reddit;
 } // namespace eXRC::Service
 
+namespace eXVHP::Service {
+class JustStreamLive;
+class Mixture;
+class Streamable;
+class Streamff;
+class Streamja;
+class Streamwo;
+} // namespace eXVHP::Service
+
 #include <QCheckBox>
 #include <QGroupBox>
+#include <QLineEdit>
 #include <QMainWindow>
 #include <QNetworkReply>
+#include <QProgressBar>
 #include <QPushButton>
+#include <QRadioButton>
 
 class AppWindow : public QMainWindow {
   Q_OBJECT
 
 private:
-  eXRC::Service::Reddit *m_redditService;
+  eXRC::Service::Reddit *m_red;
+  eXVHP::Service::JustStreamLive *m_jsl;
+  eXVHP::Service::Mixture *m_mix;
+  eXVHP::Service::Streamable *m_sab;
+  eXVHP::Service::Streamff *m_sff;
+  eXVHP::Service::Streamja *m_sja;
+  eXVHP::Service::Streamwo *m_swo;
   QCheckBox *m_permanentCB;
-  QGroupBox *m_authGB;
-  QPushButton *m_authBtn, *m_revokeBtn;
+  QGroupBox *m_authGB, *m_postGB;
+  QLineEdit *m_subredditLE, *m_titleLE, *m_flairLE;
+  QProgressBar *m_uploadProgress;
+  QPushButton *m_authBtn, *m_revokeBtn, *m_videoSelectBtn;
+  QRadioButton *m_jslRB, *m_mixRB, *m_redRB, *m_sabRB, *m_sffRB, *m_sjaRB,
+      *m_swoRB;
   void setupCentralWidget();
   void setupMenuBar();
 
@@ -34,6 +56,7 @@ private slots:
   void onRevoked();
   void onRevokeError(const QNetworkReply::NetworkError &error,
                      const QString &errorString);
+  void onVideoFileSelectAndUpload();
 };
 
 #endif // APPWINDOW_HXX
--- Application/Source/AppWindow.cxx
+++ Application/Source/AppWindow.cxx
@@ -1,45 +1,102 @@
 #include "AppWindow.hxx"
 #include "eXRC/Reddit.hxx"
+#include "eXVHP/JustStreamLive.hxx"
+#include "eXVHP/Mixture.hxx"
+#include "eXVHP/Streamable.hxx"
+#include "eXVHP/Streamff.hxx"
+#include "eXVHP/Streamja.hxx"
+#include "eXVHP/Streamwo.hxx"
 #include <QApplication>
+#include <QFileDialog>
+#include <QLabel>
+#include <QLineEdit>
 #include <QMenuBar>
 #include <QMessageBox>
 #include <QVBoxLayout>
 
 AppWindow::AppWindow(QWidget *parent) : QMainWindow(parent) {
+  // Setup services
+  QNetworkAccessManager *nam = new QNetworkAccessManager;
+  m_jsl = new eXVHP::Service::JustStreamLive(nam);
+  m_mix = new eXVHP::Service::Mixture(nam);
+  m_sab = new eXVHP::Service::Streamable(nam);
+  m_sff = new eXVHP::Service::Streamff(nam);
+  m_sja = new eXVHP::Service::Streamja(nam);
+  m_swo = new eXVHP::Service::Streamwo(nam);
+  m_red = new eXRC::Service::Reddit("bnPnumDqM7YlDueRSzZCDw", nam);
+
+  m_permanentCB = new QCheckBox("Permanent");
+  m_authGB = new QGroupBox("Reddit Authorization");
+  m_postGB = new QGroupBox("Post Video");
+  m_subredditLE = new QLineEdit;
+  m_titleLE = new QLineEdit;
+  m_flairLE = new QLineEdit;
+  m_uploadProgress = new QProgressBar;
+  m_authBtn = new QPushButton("Authorize");
+  m_revokeBtn = new QPushButton("Revoke");
+  m_videoSelectBtn = new QPushButton("Select Video File and Upload");
+  m_jslRB = new QRadioButton("JustStreamLive");
+  m_mixRB = new QRadioButton("Mixture");
+  m_redRB = new QRadioButton("Reddit");
+  m_sabRB = new QRadioButton("Streamable");
+  m_sffRB = new QRadioButton("Streamff");
+  m_sjaRB = new QRadioButton("Streamja");
+  m_swoRB = new QRadioButton("Streamwo");
+
   this->setupCentralWidget();
   this->setupMenuBar();
 }
 
 void AppWindow::setupCentralWidget() {
-  m_authBtn = new QPushButton("Authorize");
-  m_revokeBtn = new QPushButton("Revoke");
-  m_authGB = new QGroupBox("Reddit Authorization");
-  m_permanentCB = new QCheckBox("Permanent");
-  m_redditService = new eXRC::Service::Reddit("bnPnumDqM7YlDueRSzZCDw");
   QVBoxLayout *authLayout = new QVBoxLayout;
+  QHBoxLayout *postLayout = new QHBoxLayout;
+  QVBoxLayout *postHostLayout = new QVBoxLayout;
+  QVBoxLayout *postDetailsLayout = new QVBoxLayout;
   QVBoxLayout *centralLayout = new QVBoxLayout;
   QWidget *centralWidget = new QWidget;
 
-  m_authGB->setLayout(authLayout);
+  m_subredditLE->setPlaceholderText("Post Subreddit (Optional, posts to user's "
+                                    "profile if no subreddit specified)");
+  m_titleLE->setPlaceholderText("Post Title");
+  m_flairLE->setPlaceholderText("Post Flair ID (Optional)");
+
+  postHostLayout->addWidget(new QLabel("Video Host"), 0, Qt::AlignHCenter);
+  postHostLayout->addWidget(m_jslRB);
+  postHostLayout->addWidget(m_mixRB);
+  postHostLayout->addWidget(m_redRB);
+  postHostLayout->addWidget(m_sabRB);
+  postHostLayout->addWidget(m_sffRB);
+  postHostLayout->addWidget(m_sjaRB);
+  postHostLayout->addWidget(m_swoRB);
+  postLayout->addLayout(postHostLayout);
+  postDetailsLayout->addWidget(m_titleLE);
+  postDetailsLayout->addWidget(m_subredditLE);
+  postDetailsLayout->addWidget(m_flairLE);
+  postDetailsLayout->addWidget(m_videoSelectBtn);
+  postDetailsLayout->addWidget(m_uploadProgress);
+  postLayout->addLayout(postDetailsLayout, 1);
   authLayout->addWidget(m_permanentCB, 0, Qt::AlignHCenter);
   authLayout->addWidget(m_authBtn);
   authLayout->addWidget(m_revokeBtn);
-  centralLayout->addWidget(m_authGB);
-  centralLayout->addStretch();
+  m_authGB->setLayout(authLayout);
+  m_postGB->setLayout(postLayout);
 
-  m_revokeBtn->setDisabled(true);
+  onRevoked();
 
-  connect(m_redditService, &eXRC::Service::Reddit::granted, this,
-          &AppWindow::onGranted);
-  connect(m_redditService, &eXRC::Service::Reddit::grantError, this,
+  connect(m_red, &eXRC::Service::Reddit::granted, this, &AppWindow::onGranted);
+  connect(m_red, &eXRC::Service::Reddit::grantError, this,
           &AppWindow::onGrantError);
-  connect(m_redditService, &eXRC::Service::Reddit::revoked, this,
-          &AppWindow::onRevoked);
-  connect(m_redditService, &eXRC::Service::Reddit::revokeError, this,
+  connect(m_red, &eXRC::Service::Reddit::revoked, this, &AppWindow::onRevoked);
+  connect(m_red, &eXRC::Service::Reddit::revokeError, this,
           &AppWindow::onRevokeError);
   connect(m_authBtn, &QPushButton::clicked, this, &AppWindow::onAuthorize);
   connect(m_revokeBtn, &QPushButton::clicked, this, &AppWindow::onRevoke);
+  connect(m_videoSelectBtn, &QPushButton::clicked, this,
+          &AppWindow::onVideoFileSelectAndUpload);
 
+  centralLayout->addWidget(m_authGB);
+  centralLayout->addWidget(m_postGB);
+  centralLayout->addStretch();
   centralWidget->setLayout(centralLayout);
   setCentralWidget(centralWidget);
 }
@@ -60,10 +117,25 @@ void AppWindow::setupMenuBar() {
 void AppWindow::onAuthorize() {
   m_authBtn->setDisabled(true);
   m_permanentCB->setDisabled(true);
-  m_redditService->grant(m_permanentCB->isChecked());
+  m_red->grant(m_permanentCB->isChecked());
 }
 
-void AppWindow::onGranted() { m_revokeBtn->setEnabled(true); }
+void AppWindow::onGranted() {
+  m_authBtn->setDisabled(true);
+  m_revokeBtn->setEnabled(true);
+  m_videoSelectBtn->setEnabled(true);
+  m_permanentCB->setDisabled(true);
+  m_flairLE->setEnabled(true);
+  m_subredditLE->setEnabled(true);
+  m_titleLE->setEnabled(true);
+  m_jslRB->setEnabled(true);
+  m_mixRB->setEnabled(true);
+  m_redRB->setEnabled(true);
+  m_sabRB->setEnabled(true);
+  m_sffRB->setEnabled(true);
+  m_sjaRB->setEnabled(true);
+  m_swoRB->setEnabled(true);
+}
 
 void AppWindow::onGrantError(const QString &error,
                              const QString &errorDescription, const QUrl &uri) {
@@ -81,12 +153,27 @@ void AppWindow::onGrantError(const QString &error,
 
 void AppWindow::onRevoke() {
   m_revokeBtn->setDisabled(true);
-  m_redditService->revoke();
+  m_red->revoke();
 }
 
 void AppWindow::onRevoked() {
   m_authBtn->setEnabled(true);
+  m_revokeBtn->setDisabled(true);
+  m_videoSelectBtn->setDisabled(true);
   m_permanentCB->setEnabled(true);
+  m_flairLE->setText(QString());
+  m_flairLE->setDisabled(true);
+  m_subredditLE->setText(QString());
+  m_subredditLE->setDisabled(true);
+  m_titleLE->setText(QString());
+  m_titleLE->setDisabled(true);
+  m_jslRB->setDisabled(true);
+  m_mixRB->setDisabled(true);
+  m_redRB->setDisabled(true);
+  m_sabRB->setDisabled(true);
+  m_sffRB->setDisabled(true);
+  m_sjaRB->setDisabled(true);
+  m_swoRB->setDisabled(true);
 }
 
 void AppWindow::onRevokeError(const QNetworkReply::NetworkError &error,
@@ -94,4 +181,214 @@ void AppWindow::onRevokeError(const QNetworkReply::NetworkError &error,
   m_revokeBtn->setEnabled(true);
   QMessageBox::warning(this, "Error occurred while revoking token",
                        errorString);
+}
+
+void AppWindow::onVideoFileSelectAndUpload() {
+  if (m_titleLE->text().isEmpty()) {
+    QMessageBox::warning(this, "No post title specified",
+                         "A post title must be specified for a Reddit post!");
+    return;
+  }
+
+  bool hostSelected = m_jslRB->isChecked() || m_mixRB->isChecked() ||
+                      m_sabRB->isChecked() || m_sffRB->isChecked() ||
+                      m_sjaRB->isChecked() || m_swoRB->isChecked();
+
+  if (!hostSelected) {
+    QMessageBox::warning(
+        this, "No video host selected",
+        "You need to select a video host before selecting the video file!");
+    return;
+  }
+
+  QString filter;
+
+  if (m_jslRB->isChecked())
+    filter = "Supported Video Files (*.mkv *.mp4 *.webm)";
+
+  else if (m_sabRB->isChecked())
+    filter = "Supported Video Files (*.mkv *.mp4)";
+
+  else if (m_mixRB->isChecked() || m_sffRB->isChecked() ||
+           m_sjaRB->isChecked() || m_swoRB->isChecked())
+    filter = "Supported Video Files (*.mp4)";
+
+  QString videoFilePath = QFileDialog::getOpenFileName(
+      this, "Select supported video file", QDir::homePath(), filter);
+
+  if (videoFilePath.isNull())
+    return;
+
+  QFile *videoFile = new QFile(videoFilePath);
+
+  if (m_jslRB->isChecked()) {
+    QProgressBar *uploadProgress = m_uploadProgress;
+    connect(m_jsl, &eXVHP::Service::JustStreamLive::videoUploadProgress, this,
+            [uploadProgress, videoFile](QFile *vidFile, qint64 bytesSent,
+                                        qint64 bytesTotal) {
+              if (videoFile == vidFile) {
+                uploadProgress->setValue(bytesSent);
+                uploadProgress->setMaximum(bytesTotal);
+              }
+            });
+
+    connect(m_jsl, &eXVHP::Service::JustStreamLive::videoUploaded, this,
+            [this, videoFile](QFile *vidFile, const QString &linkId,
+                              const QString &videoLink) {
+              if (videoFile == vidFile) {
+                QMessageBox::information(this, "JustStreamLive Upload Success",
+                                         videoLink);
+              }
+            });
+
+    connect(m_jsl, &eXVHP::Service::JustStreamLive::videoUploadError, this,
+            [this, videoFile](QFile *vidFile, const QString &error) {
+              if (videoFile == vidFile) {
+                QMessageBox::warning(this, "JustStreamLive Upload Error",
+                                     error);
+              }
+            });
+
+    m_jsl->uploadVideo(videoFile);
+  } else if (m_mixRB->isChecked()) {
+    QProgressBar *uploadProgress = m_uploadProgress;
+    connect(m_mix, &eXVHP::Service::Mixture::videoUploadProgress, this,
+            [uploadProgress, videoFile](QFile *vidFile, qint64 bytesSent,
+                                        qint64 bytesTotal) {
+              if (videoFile == vidFile) {
+                uploadProgress->setValue(bytesSent);
+                uploadProgress->setMaximum(bytesTotal);
+              }
+            });
+
+    connect(m_mix, &eXVHP::Service::Mixture::videoUploaded, this,
+            [this, videoFile](QFile *vidFile, const QString &linkId,
+                              const QString &videoLink) {
+              if (videoFile == vidFile) {
+                QMessageBox::information(this, "Video Uploaded", videoLink);
+              }
+            });
+
+    connect(m_mix, &eXVHP::Service::Mixture::videoUploadError, this,
+            [this, videoFile](QFile *vidFile, const QString &error) {
+              if (videoFile == vidFile) {
+                QMessageBox::warning(this, "Mixture Video Upload Error", error);
+              }
+            });
+
+    m_mix->uploadVideo(videoFile);
+  } else if (m_sabRB->isChecked()) {
+    QProgressBar *uploadProgress = m_uploadProgress;
+    connect(m_sab, &eXVHP::Service::Streamable::videoUploadProgress, this,
+            [uploadProgress, videoFile](QFile *vidFile, qint64 bytesSent,
+                                        qint64 bytesTotal) {
+              if (videoFile == vidFile) {
+                uploadProgress->setValue(bytesSent);
+                uploadProgress->setMaximum(bytesTotal);
+              }
+            });
+
+    connect(m_sab, &eXVHP::Service::Streamable::videoUploaded, this,
+            [this, videoFile](QFile *vidFile, const QString &linkId,
+                              const QString &videoLink) {
+              if (videoFile == vidFile) {
+                QMessageBox::information(this, "Video Uploaded", videoLink);
+              }
+            });
+
+    connect(m_sab, &eXVHP::Service::Streamable::videoUploadError, this,
+            [this, videoFile](QFile *vidFile, const QString &error) {
+              if (videoFile == vidFile) {
+                QMessageBox::warning(this, "Streamable Video Upload Error",
+                                     error);
+              }
+            });
+
+    m_sab->uploadVideo(videoFile, m_titleLE->text(), "us-east-1");
+  } else if (m_sffRB->isChecked()) {
+    QProgressBar *uploadProgress = m_uploadProgress;
+    connect(m_sff, &eXVHP::Service::Streamff::videoUploadProgress, this,
+            [uploadProgress, videoFile](QFile *vidFile, qint64 bytesSent,
+                                        qint64 bytesTotal) {
+              if (videoFile == vidFile) {
+                uploadProgress->setValue(bytesSent);
+                uploadProgress->setMaximum(bytesTotal);
+              }
+            });
+
+    connect(m_sff, &eXVHP::Service::Streamff::videoUploaded, this,
+            [this, videoFile](QFile *vidFile, const QString &linkId,
+                              const QString &videoLink) {
+              if (videoFile == vidFile) {
+                QMessageBox::information(this, "Video Uploaded", videoLink);
+              }
+            });
+
+    connect(m_sff, &eXVHP::Service::Streamff::videoUploadError, this,
+            [this, videoFile](QFile *vidFile, const QString &error) {
+              if (videoFile == vidFile) {
+                QMessageBox::warning(this, "Streamff Video Upload Error",
+                                     error);
+              }
+            });
+
+    m_sff->uploadVideo(videoFile);
+  } else if (m_sjaRB->isChecked()) {
+    QProgressBar *uploadProgress = m_uploadProgress;
+    connect(m_sja, &eXVHP::Service::Streamja::videoUploadProgress, this,
+            [uploadProgress, videoFile](QFile *vidFile, qint64 bytesSent,
+                                        qint64 bytesTotal) {
+              if (videoFile == vidFile) {
+                uploadProgress->setValue(bytesSent);
+                uploadProgress->setMaximum(bytesTotal);
+              }
+            });
+
+    connect(m_sja, &eXVHP::Service::Streamja::videoUploaded, this,
+            [this, videoFile](QFile *vidFile, const QString &linkId,
+                              const QString &videoLink,
+                              const QString &videoEmbedLink) {
+              if (videoFile == vidFile) {
+                QMessageBox::information(this, "Video Uploaded", videoLink);
+              }
+            });
+
+    connect(m_sja, &eXVHP::Service::Streamja::videoUploadError, this,
+            [this, videoFile](QFile *vidFile, const QString &error) {
+              if (videoFile == vidFile) {
+                QMessageBox::warning(this, "Streamja Video Upload Error",
+                                     error);
+              }
+            });
+
+    m_sja->uploadVideo(videoFile);
+  } else if (m_swoRB->isChecked()) {
+    QProgressBar *uploadProgress = m_uploadProgress;
+    connect(m_swo, &eXVHP::Service::Streamwo::videoUploadProgress, this,
+            [uploadProgress, videoFile](QFile *vidFile, qint64 bytesSent,
+                                        qint64 bytesTotal) {
+              if (videoFile == vidFile) {
+                uploadProgress->setValue(bytesSent);
+                uploadProgress->setMaximum(bytesTotal);
+              }
+            });
+
+    connect(m_swo, &eXVHP::Service::Streamwo::videoUploaded, this,
+            [this, videoFile](QFile *vidFile, const QString &linkId,
+                              const QString &videoLink) {
+              if (videoFile == vidFile) {
+                QMessageBox::information(this, "Video Uploaded", videoLink);
+              }
+            });
+
+    connect(m_swo, &eXVHP::Service::Streamwo::videoUploadError, this,
+            [this, videoFile](QFile *vidFile, const QString &error) {
+              if (videoFile == vidFile) {
+                QMessageBox::warning(this, "Streamwo Video Upload Error",
+                                     error);
+              }
+            });
+
+    m_swo->uploadVideo(videoFile);
+  }
 }
